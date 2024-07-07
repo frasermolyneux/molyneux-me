252d8b3a491eb128f3acd5ba9ae3ac07f7fb0e81
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-07 16:48:45 +0000

    Add static web app resource

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,4 @@
 locals {
   resource_group_name = "rg-molyneux-me-${var.environment}-${var.location}"
+  static_web_app_name = "stapp-molyneux-me-${var.environment}-${var.location}"
 }
---
+++ terraform/static_web_app.tf
@@ -0,0 +1,12 @@
+resource "azurerm_static_web_app" "app" {
+  name                = local.static_web_app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku_tier = "Free"
+  sku_size = "Free"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
