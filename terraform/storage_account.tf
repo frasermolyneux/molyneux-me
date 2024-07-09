105b12238fedc3a5a74e2aa4e2aff6074b890ab0
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-09 21:04:44 +0100

    Add storage account for images

---
+++ terraform/storage_account.tf
@@ -0,0 +1,22 @@
+resource "azurerm_storage_account" "app_data_storage" {
+  name                = local.app_data_storage_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  account_kind             = "StorageV2"
+  access_tier              = "Hot"
+
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "images_container" {
+  name = "images"
+
+  storage_account_name  = azurerm_storage_account.app_data_storage.name
+  container_access_type = "blob"
+}
